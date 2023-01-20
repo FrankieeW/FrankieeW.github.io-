638d5174650d11aac70988ac1bed8c7179894418
FrankieeW <frankie.fc.wang@outlook.com>
2023-01-20 12:44:31 +0800

    11

--- uitil/test.py
+++ uitil/test.py
@@ -7,7 +7,6 @@
 @Date    ：20 January 2023
 """
 
-# from PIL import Image
-# im = Image.open(f'../img/1674176708000.jpeg')
-# im.save("cat-out3.jpeg", quality=15)
-print(1)
+from PIL import Image
+im = Image.open(f'../img/1674176708000.jpeg')
+im.save("cat-out3.jpeg", quality=15)
