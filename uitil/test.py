84f2a6746a09ab5ef59a8c115da30a77e439e457
FrankieeW <frankie.fc.wang@outlook.com>
2023-01-20 11:57:14 +0800

    1
    1

---
+++ uitil/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：FrankieeW.github.io- 
+@File    ：test.py
+@Author  ：Frankie
+@Date    ：20 January,2023
+"""
+
+if __name__ == '__main__':
+    pass
