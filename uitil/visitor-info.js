0b9cd0bd60e97d03286ec77de7370fe083efc66b
FrankieeW <frankie.fc.wang@outlook.com>
2023-05-03 01:41:40 +0800

    update230503

--- uitil/visitor-info.js
+++ uitil/visitor-info.js
@@ -1,34 +1,69 @@
-// 获取当前时间戳
-var currentTime = new Date().getTime();
+// 获取访问者IP地址
+function getIP(json) {
+    var visitorIP = json.ip;
 
-// 从本地存储中获取访问者信息
-var visitorInfo = JSON.parse(localStorage.getItem('../Data/visitorInfo'));
+    // 获取当前时间戳
+    var currentTime = new Date().getTime();
 
-// 如果visitorInfo不存在或者当前时间与上次记录时间相差1分钟以上，就记录访问者信息
-if (!visitorInfo || currentTime - visitorInfo.timestamp > 60 * 1000 || visitorInfo.ip !== visitorIP) {
+    // 从本地存储中获取访问者信息
+    var visitorInfo = JSON.parse(localStorage.getItem('visitorInfo'));
 
-    // 创建一个记录访问者信息的对象
-    var visitorInfo = {
-        ip: null,
-        browser: null,
-        language: null,
-        timestamp: null
-    };
+    // 如果visitorInfo不存在或者当前时间与上次记录时间相差1分钟以上，就记录访问者信息
+    if (!visitorInfo || currentTime - visitorInfo.timestamp > 60 * 1000 || visitorInfo.ip !== visitorIP) {
 
-    // 获取访问者IP地址
-    function getIP(json) {
-        visitorInfo.ip = json.ip;
+        // 创建一个记录访问者信息的对象
+        var newVisitorInfo = {
+            ip: visitorIP,
+            browser: window.navigator.userAgent,
+            language: window.navigator.language || window.navigator.userLanguage,
+            timestamp: currentTime
+        };
+
+        // 将访问者信息保存到localStorage中
+        localStorage.setItem('visitorInfo', JSON.stringify(newVisitorInfo));
+
+        // 发送POST请求将访问者信息保存到GitHub
+        var repoOwner = 'FrankieeW';
+        var repoName = 'FrankieeW.github.io-';
+        var path = 'Data/visitor.json';
+        var accessToken = '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';
+
+        var apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}`;
+
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                var fileContent = JSON.parse(atob(data.content));
+                var visitors = fileContent.visitors || [];
+
+                // 判断是否已经记录过该IP地址
+                var existingVisitor = visitors.find(visitor => visitor.ip === visitorIP);
+
+                if (!existingVisitor) {
+                    visitors.push(newVisitorInfo);
+                }
+
+                // 发送PUT请求将更新后的访问者信息保存到GitHub
+                var body = {
+                    message: 'Update visitors.json',
+                    content: btoa(JSON.stringify({ visitors: visitors }, null, 2)),
+                    sha: data.sha
+                };
+
+                fetch(apiUrl, {
+                    method: 'PUT',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(body)
+                });
+            });
     }
-
-    // 获取访问者浏览器信息
-    visitorInfo.browser = window.navigator.userAgent;
-
-    // 获取访问者语言设置
-    visitorInfo.language = window.navigator.language || window.navigator.userLanguage;
-
-    // 记录访问时间戳
-    visitorInfo.timestamp = currentTime;
-
-    // 将访问者信息保存到localStorage中
-    localStorage.setItem('../Data/visitorInfo', JSON.stringify(visitorInfo));
 }
